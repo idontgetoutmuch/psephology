e24b4cb5878043bf0fb9cb97b3b80faf18fef660
Dominic Steinitz <dominic@steinitz.org>
2018-05-11 18:16:30 +0200

    Just draw the wards

--- TestR.hs
+++ TestR.hs
@@ -1,23 +1,8 @@
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
+{-# OPTIONS_GHC -Wall                    #-}
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
-{-# LANGUAGE DataKinds,
- FlexibleContexts,
- TemplateHaskell,
- TypeOperators,
- TypeSynonymInstances,
- FlexibleInstances,
- QuasiQuotes,
- UndecidableInstances,
- ExplicitForAll,
- ScopedTypeVariables,
- OverloadedStrings,
- GADTs,
- TypeApplications
-#-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes     #-}
 
 module Main (main) where
 
@@ -31,40 +16,29 @@ import System.FilePath
 
 import Control.Monad
 
+root :: FilePath
+root = "data/KingstonuponThames"
+
 main :: IO ()
 main = do
-  ds <- getDirectoryContents "data//KingstonuponThames"
+  ds <- getDirectoryContents root
   let es = filter (\x -> takeExtension x == ".shp") ds
-  putStrLn $ show es
   R.runRegion $ do
     [r| library(rgdal) |]
     [r| library(ggplot2) |]
-    let foo :: String
-        foo = "data//KingstonuponThames" </> (es!!0)
-    shapefile <- [r| readOGR(foo_hs) |]
-    shapefile_df <- [r| fortify(shapefile_hs) |]
-    let foo1 :: String
-        foo1 = "data//KingstonuponThames" </> (es!!1)
-    shapefile1 <- [r| readOGR(foo1_hs) |]
-    shapefile1_df <- [r| fortify(shapefile1_hs) |]
-    map1 <- [r| ggplot() +
-                geom_polygon(data = shapefile_df_hs, 
-                          aes(x = long, y = lat, group = group),
-                          color = 'gray', fill = 'blue', size = .2) +
-                geom_polygon(data = shapefile1_df_hs, 
-                          aes(x = long, y = lat, group = group),
-                          color = 'gray', fill = 'blue', size = .2) |]
+    map0 <- [r| ggplot() |]
     mapN <- foldM
-      (\m f -> do let bar :: String
-                      bar = "data/KingstonuponThames" </> f
-                  shapefileN <- [r| readOGR(bar_hs) |]
+      (\m f -> do let ward = root </> f
+                  shapefileN <- [r| readOGR(ward_hs) |]
                   shapefileN_df <- [r| fortify(shapefileN_hs) |]
                   [r| m_hs + 
                       geom_polygon(data = shapefileN_df_hs, 
                                    aes(x = long, y = lat, group = group),
-                                   color = 'gray', fill = 'blue', size = .2) |]) map1 (drop 2 es)
+                                   color = 'gray', fill = 'blue', size = .2) |]) map0 es
     map_projected <- [r| mapN_hs + coord_map() |]
+    [r| png(filename="diagrams/kingston.png") |]
     [r| print(map_projected_hs) |]
+    [r| dev.off() |]
     return ()
   putStrLn "Finished"
 
